01a348983946b9d7e021c5b00d1d16b829e5b979
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-09 20:44:23 -0700

    Setting Page Update

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -3,8 +3,22 @@ export default function SettingsPage(){
         <div>
             <h1>Settings</h1>
             <div>
-                <button>Light Theme</button>
-                <button>Dark Theme</button>
+                <h3>Text Size</h3>
+                <div>
+                    <button>10px</button>
+                    <button>12px</button>
+                    <button>14px</button>
+                    <button>16px</button>
+                    <button>24px</button>
+                </div>
+                <p>"Sample Text"</p>
+            </div>
+            <div>
+                <h3>Color Settings</h3>
+                <div>
+                    <button>Light Theme</button>
+                    <button>Dark Theme</button>
+                </div>
             </div>
         </div>
     )
