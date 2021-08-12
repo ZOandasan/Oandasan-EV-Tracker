a34aeda6442ba4432d4e31320b4a4e8be1661c56
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-12 09:37:33 -0700

    Favicon and Details update

--- src/components/PokemonCardDetail/PokemonCardDetail.jsx
+++ src/components/PokemonCardDetail/PokemonCardDetail.jsx
@@ -2,7 +2,19 @@ export default function PokemonCardDetail({pokemon}){
     return (
     <>
         <div>
-            <span>HP: {pokemon.hpEV} | ATK: {pokemon.atkEV} | DEF: {pokemon.defEV} | Sp.ATK: {pokemon.spAtkEV} | Sp.Def: {pokemon.spDefEV} | SPD: {pokemon.spdEV} </span>
+            <span>
+                <button>+10</button> <button>+1</button> HP: {pokemon.hpEV} <button>-1</button> <button>-10</button>
+                <br/> 
+                <button>+10</button> <button>+1</button> ATK: {pokemon.atkEV} <button>-1</button> <button>-10</button>
+                <br/> 
+                <button>+10</button> <button>+1</button> DEF: {pokemon.defEV} <button>-1</button> <button>-10</button>
+                <br/> 
+                <button>+10</button> <button>+1</button> Sp.ATK: {pokemon.spAtkEV} <button>-1</button> <button>-10</button>
+                <br/> 
+                <button>+10</button> <button>+1</button> Sp.Def: {pokemon.spDefEV} <button>-1</button> <button>-10</button>
+                <br/> 
+                <button>+10</button> <button>+1</button> SPD: {pokemon.spdEV} <button>-1</button> <button>-10</button>
+            </span>
         </div>
     </>
     );
