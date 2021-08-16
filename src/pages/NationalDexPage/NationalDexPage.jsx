db2ccaa8a1b684701fd49636cd1bc6eb761990f2
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-16 15:29:27 -0700

    National Dex Page Functionality

--- src/pages/NationalDexPage/NationalDexPage.jsx
+++ src/pages/NationalDexPage/NationalDexPage.jsx
@@ -1,5 +1,33 @@
+import { set } from 'mongoose';
+import { useState } from 'react';
 export default function NationalDex() {
+  const [page, setPage] = useState(1);
+  const [start, setStart] = useState(1);
+  const [end, setEnd] = useState(20);
+  
+  function changePage(evt){
+    setPage(evt.target.value);
+    let newStart = (20*(page - 1)) + 1;
+    setStart(newStart);
+    setEnd(newStart + 19);
+  }
+
+  let initSelect = [];
+  for (let idx = 1; 42 > idx; ++idx){
+    initSelect.push (
+      <option value={`${idx}`}>{idx}</option>
+    );
+  }
+
   return (
-    <h1>National Dex</h1>
+    <div>
+      <h1>National Dex</h1>
+      <h3>Dex Numbers: {(20*(page - 1)) + 1}-{20*(page)}</h3>
+        <select name="page" onChange={changePage}>
+          {initSelect.map((o, idx) =>(
+            <>{o}</>
+          ))}
+        </select> 
+    </div>
   );
 }
