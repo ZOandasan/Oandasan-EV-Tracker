ff17e9585c505e66090575a01a64140902f943f5
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-17 10:53:37 -0700

    Styling Settings/Account

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -8,15 +8,17 @@ export default function AccountPage({user, setUser}){
         setUser(null);
     }
     return (
-        <div>
-            <h1>Account Page</h1>
+        <div className="settings">
+            <h1 className="head">Account Page</h1>
             <div>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
+                <p>User: {user.name}</p>
+                <p>Email: {user.email}</p>
                 <p>Not you? <Link to="" onClick={handleLogOut}>Log Out</Link></p>
             </div>
+            <hr />
+            <h2>Terms of Service:</h2>
+            <hr />
             <div>
-                <h2>Terms of Service:</h2>
                 <p>Text Here:</p>
             </div>
         </div>
--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -1,20 +1,23 @@
 export default function SettingsPage(){
     return (
-        <div>
-            <h1>Settings</h1>
+        <div className="settings">
+            <h1 className="head">Settings</h1>
             <div>
                 <h3>Text Size</h3>
+                <hr />
                 <div>
                     <button>10px</button>
                     <button>12px</button>
                     <button>14px</button>
                     <button>16px</button>
                     <button>24px</button>
+                    <p>"Sample Text"</p>
                 </div>
-                <p>"Sample Text"</p>
             </div>
             <div>
+                <hr />
                 <h3>Color Settings</h3>
+                <hr />
                 <div>
                     <button>Light Theme</button>
                     <button>Dark Theme</button>
